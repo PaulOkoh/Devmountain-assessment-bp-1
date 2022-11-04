3dce8500035bf6d37fe2116c07620d52b1a4e60f
Paul Okoh <pauldokoh@gmail.com>
2022-11-04 14:53:04 -0500

    add answers for 'gitInfo.js'

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,11 @@
 
 //CODE HERE
 
+let gitDefinition =
+  "Git is a program you can install on your computer that allows tracking of changes made to code projects. It takes snapshots of changes as they are made during a project. Git is also referred to as a version control system, you can view the whole history of the project and different individuals or teams can contribute to same project. A projects in git is called a repository;";
+
+console.log(gitDefinition);
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -19,6 +24,10 @@
 
 //CODE HERE
 
+let gitHubDefinition =
+  "Website where git repositories are stored. Repositories can be uploaded and downloaded from GitHub. GitHub could be referred to as a cloud service for git repositories";
+
+console.log(gitHubDefinition);
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,6 +35,11 @@
 */
 
 //CODE HERE
+
+let gitInitDefinition =
+  "git init initializes a local repository by adding a '.git' tag to that repository, this allows changes made to the repository to be tracked by git.";
+
+console.log(gitInitDefinition);
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -35,6 +49,11 @@
 
 //CODE HERE
 
+let gitCloneDefinition =
+  "'git clone' copies a repository from git hub and puts that repository on your local machine / computer";
+
+console.log(gitCloneDefinition);
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +61,11 @@
 */
 
 //CODE HERE
+
+let gitStatusDefinition =
+  "Gives a summary of the state of a repository, shows which changes have been staged";
+
+console.log(gitStatusDefinition);
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -54,6 +78,15 @@
 
 //CODE HERE
 
+let gitAddDefinition =
+  "'git add' adds  updated/modified files to the staging area awaiting the 'git commit'";
+
+console.log(gitAddDefinition);
+
+let gitAddCode = "git add .";
+
+console.log(gitAddCode);
+
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -65,6 +98,10 @@
 
 //CODE HERE
 
+let gitCommitDefinition =
+  "Creates a snapshot of the changes that have been made to files that are being tracked by git";
+
+console.log(gitCommitDefinition);
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +110,7 @@
 */
 
 //CODE HERE
+
+let gitPushDefinition = " Transfers commits from your local machine to GitHub";
+
+console.log(gitPushDefinition);
