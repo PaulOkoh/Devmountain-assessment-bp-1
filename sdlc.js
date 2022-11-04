13ec834ee58102b6f63d579b52eba263c743706a
Paul Okoh <pauldokoh@gmail.com>
2022-11-04 15:41:55 -0500

    add answers for 'sdlc.js'

--- sdlc.js
+++ sdlc.js
@@ -11,11 +11,50 @@
 // Analysis of Requirements
 // Testing & Integration
 // Planning
+/**
+ * Steps in order:
+ * Planning
+ * Analysis of Requirements
+ * Design
+ * Implementation
+ * Testing & Integration
+ * Maintain
+ */
 
+let sdlcSteps = [
+  "Planning",
+  "Analysis of Requirements",
+  "Design",
+  "Implementation",
+  "Testing & Integration",
+  "Maintain",
+];
+
+console.log(sdlcSteps);
 
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning =
+  "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+console.log(planning);
+
+const analysisAndRequirements =
+  "In this step,user requirements are analyzed and documented";
+
+console.log(analysisAndRequirements);
+
+const design =
+  "A design is developed to provide solutions to that will satisfy the requirements";
+
+const implementation =
+  "Code is constructed and reviewed by the team of developers in order to catch any bugs or problems early";
+
+const testingAndIntegration =
+  "Tests are performed with preplanned test cases or scenarios";
+
+const maintain =
+  "In this phase, the project is managed and measured to maintain quality and delivery througout the lifecyle of the application";
+console.log(maintain);
